dda8d8c04a199ee28368d0237cf4bc7f6f9866e5
Sergio <sps169@outlook.es>
2021-12-13 18:17:48 +0100

    main

--- DataAccessSLJPA/src/main/java/Main.java
+++ DataAccessSLJPA/src/main/java/Main.java
@@ -1,13 +1,18 @@
-import jpa.controllers.CommitController;
+import jpa.controllers.*;
 
 public class Main {
     public static void main(String[] args){
         Database.checkService();
-        try {
-            CommitController controller = CommitController.getInstance();
-            System.out.println(controller.getAllCommitsJSON());
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
+        CommitController commitController = CommitController.getInstance();
+        System.out.println(commitController.getAllCommitsJSON());
+
+        BossHistoryController bossHistoryController = BossHistoryController.getInstance();
+        System.out.println(bossHistoryController.getAllBossHistoriesJSON());
+
+        DepartmentController departmentController = DepartmentController.getInstance();
+        System.out.println(departmentController.getAllDepartmentsJSON());
+
+        IssueAssignmentController issueAssignmentController = IssueAssignmentController.getInstance();
+        System.out.println(issueAssignmentController.getAllIssueAssignmentsJSON());
     }
 }
