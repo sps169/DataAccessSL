de75b97a9ca1e2711b3471f90e512b0243f4c640
Sergio <sps169@outlook.es>
2021-12-14 03:14:25 +0100

    word + pequeños cambios en el diagrama

--- DataAccessSLJPA/src/main/java/Main.java
+++ DataAccessSLJPA/src/main/java/Main.java
@@ -11,48 +11,41 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args){
-//        Database.checkService();
-//        CommitController commitController = CommitController.getInstance();
-//        System.out.println("Commits");
-//        System.out.println(commitController.getAllCommitsJSON());
-//
-//        BossHistoryController bossHistoryController = BossHistoryController.getInstance();
-//        System.out.println("Boss Histories");
-//        System.out.println(bossHistoryController.getAllBossHistoriesJSON());
-//
-//        DepartmentController departmentController = DepartmentController.getInstance();
-//        System.out.println("Departments");
-//        System.out.println(departmentController.getAllDepartmentsJSON());
-//
-//        IssueAssignmentController issueAssignmentController = IssueAssignmentController.getInstance();
-//        System.out.println("Issue Assignments");
-//        System.out.println(issueAssignmentController.getAllIssueAssignmentsJSON());
-//
-//        IssueController issueController = IssueController.getInstance();
-//        System.out.println("Issues");
-//        System.out.println(issueController.getAllIssuesJSON());
-//
-//        ProgrammerController programmerController = ProgrammerController.getInstance();
-//        System.out.println("Programmers");
-//        System.out.println(programmerController.getAllProgrammersJSON());
-//
-//        ProjectAssignmentController projectAssignmentController = ProjectAssignmentController.getInstance();
-//        System.out.println("Project Assignments");
-//        System.out.println(projectAssignmentController.getAllProjectAssignmentsJSON());
-//
-//        ProjectController projectController = ProjectController.getInstance();
-//        System.out.println("Projects");
-//        System.out.println(projectController.getAllProjectsJSON());
+        Database.checkService();
+        CommitController commitController = CommitController.getInstance();
+        System.out.println("Commits");
+        System.out.println(commitController.getAllCommitsJSON());
 
-        RepositoryService repositoryService = new RepositoryService(new RepositoryRepo());
-        try {
-            repositoryService.getAllRepositories().forEach(System.out::println);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        BossHistoryController bossHistoryController = BossHistoryController.getInstance();
+        System.out.println("Boss Histories");
+        System.out.println(bossHistoryController.getAllBossHistoriesJSON());
+
+        DepartmentController departmentController = DepartmentController.getInstance();
+        System.out.println("Departments");
+        System.out.println(departmentController.getAllDepartmentsJSON());
+
+        IssueAssignmentController issueAssignmentController = IssueAssignmentController.getInstance();
+        System.out.println("Issue Assignments");
+        System.out.println(issueAssignmentController.getAllIssueAssignmentsJSON());
+
+        IssueController issueController = IssueController.getInstance();
+        System.out.println("Issues");
+        System.out.println(issueController.getAllIssuesJSON());
+
+        ProgrammerController programmerController = ProgrammerController.getInstance();
+        System.out.println("Programmers");
+        System.out.println(programmerController.getAllProgrammersJSON());
+
+        ProjectAssignmentController projectAssignmentController = ProjectAssignmentController.getInstance();
+        System.out.println("Project Assignments");
+        System.out.println(projectAssignmentController.getAllProjectAssignmentsJSON());
+
+        ProjectController projectController = ProjectController.getInstance();
+        System.out.println("Projects");
+        System.out.println(projectController.getAllProjectsJSON());
 
         RepositoryController repositoryController = RepositoryController.getInstance();
-        System.out.println("Reposn't");
-        System.out.println(repositoryController.getAllRepositoriesJSON());
+        System.out.println("Repos");
+        System.out.println(repositoryController.getRepositoryByIdJSON(1L));
     }
 }
